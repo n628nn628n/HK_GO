ddeb11bb624af37e18aa8d097d32c974cc27f39d
David_Chen <david_chen@mail.chungyo.net>
2018-07-11 10:19:34 +0800

    week1 HW

--- WK/week1.go
+++ WK/week1.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 
-	fmt.Println("hello")
+	var a int = 1
+
+	var b int32 = 2
+
+	var c int64 = 3
+
+	var d string = "999"
+
+	var e float32 = 88.8
+
+	var f float64 = 99.9
+
+	var x string = "I Love Golang_"
+
+	fmt.Printf("a + b = (%T) %d", a+int(b), a+int(b))
+	fmt.Println("")
+	fmt.Printf("a + b + c = (%T) %d", a+int(b)+int(c), a+int(b)+int(c))
+	fmt.Println("")
+	fmt.Printf("f / e = (%T) %f", float32(f)/e, float32(f)/e)
+	fmt.Println("")
+	i, _ := strconv.Atoi(d)
+	fmt.Printf("f / e = (%T) %d", a+i, a+i)
+	fmt.Println("")
+	z := strconv.Itoa(a)
+	fmt.Printf("%s%s", x, z)
+	fmt.Println("")
+	fmt.Printf("%s%d", x, a)
 }
